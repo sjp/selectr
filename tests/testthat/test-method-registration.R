c2091d74dfc687d31db12be3add8cd6596fdbad4
Simon Potter <simon@sjp.co.nz>
2026-06-05 12:46:57 +1000

    Add assertions to method registration check to avoid being skipped by testthat

--- tests/testthat/test-method-registration.R
+++ tests/testthat/test-method-registration.R
@@ -7,6 +7,13 @@ test_that("method registration occurs correctly", {
     library(xml2)
     x2doc <- read_xml("<svg><circle /></svg>")
 
-    results <- querySelector(xdoc, "circle")
-    results <- querySelector(x2doc, "circle")
+    # querySelector() must dispatch to the methods registered for each
+    # package's document class and find the element
+    res_xml <- querySelector(xdoc, "circle")
+    expect_true(inherits(res_xml, "XMLInternalNode"))
+    expect_that(xmlName(res_xml), equals("circle"))
+
+    res_xml2 <- querySelector(x2doc, "circle")
+    expect_true(inherits(res_xml2, "xml_node"))
+    expect_that(xml_name(res_xml2), equals("circle"))
 })
